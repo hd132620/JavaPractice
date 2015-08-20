b1fdf2329801df4b159918522615c90108a2c864
hd132620 <rkwk2554@naver.com>
2015-08-20 11:34:00 +0900

    실습과제 1 업그레이드

--- JavaPractice/src/org/dimigo/inheritance/Figure.java
+++ JavaPractice/src/org/dimigo/inheritance/Figure.java
@@ -28,5 +28,14 @@ public class Figure {
 	protected double calcArea() {
 		return 0.0;
 	}
+	
+	protected void printCenter() {
+		System.out.println("중심좌표 : (" + centerX + ", " + centerY + ")");
+	}
+	
+	protected void moveFigure(int x, int y) {
+		this.centerX += x;
+		this.centerY += y;
+	}
 
 }
--- JavaPractice/src/org/dimigo/inheritance/FigureTest.java
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -22,9 +22,26 @@ public class FigureTest {
 		Triangle t = new Triangle(10, 10, 5, 8);
 		Rectangle r= new Rectangle(20, 20, 5, 8);
 		
-		System.out.println("Circle의 넓이 : " + c.calcArea());
-		System.out.println("Triangle의 넓이 : " + t.calcArea());
-		System.out.println("Rectangle의 넓이 : " + r.calcArea());
-
+		System.out.println("원의 넓이 : " + c.calcArea());
+		System.out.println("삼각형의 넓이 : " + t.calcArea());
+		System.out.println("사각형의 넓이 : " + r.calcArea());
+		System.out.println();
+		
+		c.printCenter();
+		t.printCenter();
+		r.printCenter();
+		System.out.println();
+		
+		System.out.println("-- 중심좌표 이동 (x, y축 5씩)");
+		System.out.println();
+		
+		c.moveFigure(5, 5);
+		t.moveFigure(5, 5);
+		r.moveFigure(5, 5);
+		
+		c.printCenter();
+		t.printCenter();
+		r.printCenter();
+		
 	}
 }
