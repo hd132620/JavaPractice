a853377f4fd69057217c8e672967b2c0c6243b99
hd132620 <rkwk2554@naver.com>
2015-09-24 11:41:26 +0900

    .

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -19,17 +19,22 @@ public class MovieTest {
 
 	public static void main(String[] args) {
 		
-		try {
+//		try {
 			Movie[] movies = { new Movie("앤트맨", 12), new Movie("사도", 12),
 					new Movie("베테랑", 15)};
 			int age = 13;
 			
-			for(Movie movie : movies) 
-				buyTicket(movie, age);
+			for(Movie movie : movies)
+				try {
+					buyTicket(movie, age);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
 			
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
+//		} catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 
 	}
 	
