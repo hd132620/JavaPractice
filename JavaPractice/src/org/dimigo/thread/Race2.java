b3eba9183042417b881bf6e5bbe34df1c952df28
hd132620 <rkwk2554@naver.com>
2015-11-05 11:42:12 +0900

    실습과제 10 완료

---
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *	 	|_ Race
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ * 
+ * @author		: 이은찬
+ * @version		: 1.0
+ */
+public class Race2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("main thread start");
+		
+		Thread r1 = new Thread(new Runner2("홍길동"));
+		Thread r2 = new Thread(new Runner2("홍길순"));
+		
+		r1.setPriority(Thread.MAX_PRIORITY);
+		r2.setPriority(Thread.MIN_PRIORITY);
+		
+		r1.start();
+		r2.start();
+		
+		System.out.println("main thread end");
+
+	}
+
+}
