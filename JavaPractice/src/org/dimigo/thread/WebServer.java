8f3b9d045c7b4f5250855f7df93241a374b81a7a
hd132620 <rkwk2554@naver.com>
2015-11-16 09:18:07 +0900

    실습과제11 완료

--- JavaPractice/src/org/dimigo/thread/WebServer.java
+++ JavaPractice/src/org/dimigo/thread/WebServer.java
@@ -30,7 +30,7 @@ public class WebServer {
 	        // 반복문을 돌면서 클라이언트의 접속을 받는다.
 	        while((connectionSocket = listenSocket.accept()) != null) {
 	            // 스레드를 생성하여 실행한다.
-	            serverThread = new Thread(new ServerThread2(connectionSocket));
+	            serverThread = new Thread(new ServerThread3(connectionSocket));
 	            serverThread.start();
 	        }
 	        
